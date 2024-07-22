ee5997aa4902951c8c2b7c5d235a111c4ca31fb3
Learsi5 <lewislearsi5@gmail.com>
2024-07-22 12:53:09 -0500

    capestone project files

--- 3.1/3.1.4-Parameters.js
+++ 3.1/3.1.4-Parameters.js
@@ -29,14 +29,3 @@ functionwithtwoParams(5);
 // prints
 // first param is: 5
 // second param is: undefined
-
-// change the function to set default values for the parameters
-
-// again, invoke the function and pass in only one number
-
-
-// Rest Operator
-// add a rest operator to the function's parameters
-// add a line to the function's body to print the value of the rest operator
-
-// again, invoke the function and pass in more than two numbers
